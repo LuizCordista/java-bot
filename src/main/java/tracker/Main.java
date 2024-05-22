8e0fcb42ca63c600e462fde6639a390d42d427be
luizcordista <luizcordista@gmail.com>
2024-05-22 15:18:18 -0300

    added .env for tokens

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -1,5 +1,6 @@
 package tracker;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
@@ -11,8 +12,9 @@ import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        Dotenv dotenv = Dotenv.load();
 
-        String token = System.getenv("DISCORD_TOKEN");
+        String token = dotenv.get("DISCORD_TOKEN");
         JDA jda = JDABuilder.createDefault(token)
                 .addEventListeners(new getPlayer())
                 .enableIntents(GatewayIntent.GUILD_MESSAGES)
